7fd7bf1f2c6b55d9d61092a5167559d7539e7159
Jiří Stránský <jistr@jistr.com>
2024-06-07 23:46:51 +0200

    Fix build.sh for Fedora

--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@
 
 set -euxo pipefail
 
-dnf -y install 'dnf-command(config-manager)'
-dnf config-manager --set-enabled powertools
-
-# CentOS image comes cleaned of locales, reinstall them
+# Container images can come cleaned of locales, reinstall them
 dnf -y reinstall glibc-common
-dnf -y install epel-release
 
 dnf -y install awstats httpd mod_ssl
 dnf clean all
